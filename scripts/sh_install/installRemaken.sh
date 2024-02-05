bffb9fe028d57ef3c52c6737244baa4c4a2d2e91
Stephane Leduc <stephane.leduc@b-com.com>
2024-02-05 16:56:02 +0100

    [script] update script for download and install remaken by sh

--- scripts/sh_install/installRemaken.sh
+++ scripts/sh_install/installRemaken.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/b-com-software-basis/remaken/releases/download/1.10.0-pre-release2/remaken-ubuntu22.04
+wget https://github.com/b-com-software-basis/remaken/releases/download/1.10.0%2Funixes/remaken-ubuntu22.04
 
 sudo chmod +x remaken-ubuntu22.04
 sudo mv remaken-ubuntu22.04 /usr/local/bin/remaken
