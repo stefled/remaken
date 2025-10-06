b719efe1283ae1343fce1885b74b8298229a099e
Stéphane Leduc <stephane.leduc@b-com.com>
2025-10-06 14:54:41 +0200

    fix: disable tag suffix for github dependencies

--- src/retrievers/AbstractFileRetriever.cpp
+++ src/retrievers/AbstractFileRetriever.cpp
@@ -33,7 +33,10 @@ std::string AbstractFileRetriever::computeSourcePath( const Dependency &  depend
     std::string sourcePath = dependency.getBaseRepository();
     sourcePath += "/" + dependency.getPackageName();
     sourcePath += "/" + dependency.getVersion();
-    sourcePath += !m_options.getTag().empty() ? "-" + m_options.getTag() : "";
+    // Exception of branch for github dependencies
+    if (dependency.getBaseRepository().find("github.com") == std::string::npos) {
+        sourcePath += !m_options.getTag().empty() ? "-" + m_options.getTag() : "";
+    }
     sourcePath += "/" + m_options.getOS();
     sourcePath += "/" + dependency.getPackageName();
     sourcePath += "_" + dependency.getVersion();
